dd32edac448cf638f47de110ff2c7fe66382f15e
karolinarm08 <159954535+karolinarm08@users.noreply.github.com>
2025-12-01 15:57:03 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,19 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'phplab'); 
-define('DB_USER', 'root');   
-define('DB_PASS', '');      
+$host = 'sql100.infinityfree.com';
+$db   = 'if0_40472805_phplab';
+$user = 'if0_40472805';
+$pass = 'dcmRXnx3yUO78';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Помилка підключення до бази даних: " . $e->getMessage());
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
